7db2a6aeb27f2415774488f6b624441efd993347
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2025-01-28 09:14:29 +0530

    add Server.ts

--- Server.ts
+++ Server.ts
@@ -1,2 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+
+const app = express();
+app.use(express.json())
+app.use(cors())
+
+app.listen(8080,()=>{
+    console.log("Server running on port 8080")
+})
